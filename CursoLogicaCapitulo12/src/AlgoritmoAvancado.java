2df33a2c8f72bc01f45fbe0c6cc484976203d98b
Mgabrielq <networkusabrasil@gmail.com>
2024-04-03 18:19:17 -0400

    first commit

---
+++ CursoLogicaCapitulo12/src/AlgoritmoAvancado.java
@@ -0,0 +1,52 @@
+
+public class AlgoritmoAvancado {
+
+	public static void main(String[] args) {
+		ListaAlunos lista = new ListaAlunos();
+		
+		Aluno aluno1 = new Aluno();
+		aluno1.nome ="Joao";
+		lista.adicionar(aluno1);
+		
+		Aluno aluno2 = new Aluno();
+		aluno2.nome ="Maria";
+		lista.adicionar(aluno2);
+		
+		Aluno aluno3 = new Aluno();
+		aluno3.nome ="Alexandre";
+		lista.adicionar(aluno3);
+		
+		Aluno aluno4 = new Aluno();
+		aluno4.nome ="Normandes";
+		lista.adicionar(aluno4);
+		
+		Aluno aluno5 = new Aluno();
+		aluno5.nome ="Tiago";
+		lista.adicionar(aluno5);
+		
+		for (int i = 0; i < lista.tamanho(); i++) {
+			Aluno a = lista.obter(i);
+			if (a != null ) {
+				System.out.println("Aluno: " + a.nome);
+			} else {
+			    System.out.println("Aluno sem Nome " );
+		}
+		}
+		
+		System.out.println("---------------------------");
+		
+		lista.remover(aluno3);
+	
+		
+	for (int i = 0; i < lista.tamanho(); i++) {
+		Aluno a = lista.obter(i);
+		if (a != null ) {
+			System.out.println("Aluno: " + a.nome);
+		} else {
+		    System.out.println("Aluno sem Nome " );
+      }
+		
+	}
+
+   }
+}
